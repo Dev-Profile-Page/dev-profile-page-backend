85a4468d6c102ce550fa3120a57dfc38f3b39eb3
Hemath DJ <djhemath@gmail.com>
2023-11-22 08:28:50 +0530

    feat: add global env vars

---
+++ environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      STACKOVERFLOW_CLIENT_ID: string;
+      STACKOVERFLOW_CLIENT_SECRET: string;
+      STACKOVERFLOW_CLIENT_REDIRECT_URI: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
