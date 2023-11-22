1d146fedf38e884a333287c8de943307f0ed30f0
Hemath DJ <djhemath@gmail.com>
2023-11-22 08:29:42 +0530

    feat: auth routes

---
+++ routers/auth.routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { handleStackOverflowCallback } from '../controllers/auth.controller';
+
+const router = express.Router();
+
+router.get('/stackoverflow', handleStackOverflowCallback);
+
+export default router;
