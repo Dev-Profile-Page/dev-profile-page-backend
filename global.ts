9749c4ce11fcc174cb5bb26e5d0bea4ccd0d989e
Hemath DJ <djhemath@gmail.com>
2023-12-14 12:24:15 +0530

    feat: add stackexchange related values and access token priv key

--- global.ts
+++ global.ts
@@ -1,3 +1,9 @@
 export const STACKOVERFLOW_CLIENT_ID=process.env.STACKOVERFLOW_CLIENT_ID;
 export const STACKOVERFLOW_CLIENT_SECRET=process.env.STACKOVERFLOW_CLIENT_SECRET;
 export const STACKOVERFLOW_CLIENT_REDIRECT_URI=process.env.STACKOVERFLOW_CLIENT_REDIRECT_URI;
+export const STACKEXCHANGE_ROOT_URL='https://api.stackexchange.com/2.3';
+export const STACKEXCHANGE_SITE_KEY='JHEj7XSa7z6EWLBsymOt3A((';
+export const STACKEXCHANE_SITE='stackoverflow';
+export const STACKEXCHANGE_USER_FILTER='!gjt2DKT4745_z6-Cgo9MGQ4B21czXHlqrB2'; // It only fetches specific data of an user
+
+export const ACCESS_TOKEN_PRIVATE_KEY = process.env.ACCESS_TOKEN_PRIVATE_KEY;
